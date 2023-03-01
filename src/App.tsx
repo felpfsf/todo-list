babbf589ebca2766e587f549fd61c6f508b38390
Felipe <feferreira.sf@gmail.com>
2023-03-01 20:24:17 -0300

    feat: task container task empty container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AddTask } from "./components/AddTask";
 import { Header } from "./components/Header";
+import { TaskContainer } from "./components/TaskContainer";
 
 function App() {
   return (
     <div>
       <Header />
       <AddTask />
+      <TaskContainer />
     </div>
   );
 }
