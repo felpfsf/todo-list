a7568bfa0fe421158023d272352c7f2168ac37e7
Felipe <feferreira.sf@gmail.com>
2023-03-01 23:28:41 -0300

    feat: task card

---
+++ src/components/TaskCard.tsx
@@ -0,0 +1,40 @@
+import { CheckCircle, Circle, Trash } from "phosphor-react";
+import { useState } from "react";
+import { TaskCardProps } from "../types";
+import styles from "./TaskCard.module.css";
+
+export const TaskCard = ({ task, onCheck, onDelete }: TaskCardProps) => {
+  const [checked, setChecked] = useState(false);
+  console.log(task.completed);
+
+  const handleCheck = () => {
+    setChecked((prev) => !prev);
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+  };
+  return (
+    <div className={styles.task_card}>
+      <button className={styles.task_check} onClick={onCheck}>
+        {task.completed ? (
+          <CheckCircle size={24} weight='fill' />
+        ) : (
+          <Circle size={24} />
+        )}
+      </button>
+      <div className={styles.task_card__content}>
+        <p
+          className={`${styles.task_text} ${
+            task.completed ? styles.task_checked : ""
+          }`}
+        >
+          {task.description}
+        </p>
+      </div>
+      <button className={styles.task_delete} onClick={onDelete}>
+        <Trash size={24} />
+      </button>
+    </div>
+  );
+};
