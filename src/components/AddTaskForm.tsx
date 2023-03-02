5cb8744f9784e951011cb5767737b5e06db2a244
Felipe <feferreira.sf@gmail.com>
2023-03-01 23:28:27 -0300

    refactor: add task form refactored with props

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -1,17 +1,39 @@
 import { PlusCircle } from "phosphor-react";
+import { useState } from "react";
+import { AddTaskFormProps } from "../types";
 import styles from "./AddTaskForm.module.css";
 
-export const AddTaskForm = () => {
+export const AddTaskForm = ({ onSubmit }: AddTaskFormProps) => {
+  const [description, setDescription] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
+  };
+
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
+    console.log(description);
+    if (description.trim()) {
+      onSubmit(description);
+      setDescription("");
+    }
   };
+
   return (
-    <form className={styles.task_form} onSubmit={handleSubmit}>
-      <input type='text' placeholder='Adicionar nova tarefa' />
-      <button>
-        Criar
-        <PlusCircle />
-      </button>
-    </form>
+    <section className='wrapper'>
+      <form className={`${styles.task_form}`} onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Adicionar nova tarefa'
+          aria-label='Adicionar nova tarefa'
+          value={description}
+          onChange={handleChange}
+        />
+        <button type='submit' aria-label='Criar Tarefa'>
+          Criar
+          <PlusCircle />
+        </button>
+      </form>
+    </section>
   );
 };
