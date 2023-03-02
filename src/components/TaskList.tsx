3452c9a93e310302a46d2a9cd4cabb1da1bc3820
Felipe <feferreira.sf@gmail.com>
2023-03-01 23:29:46 -0300

    feat: task list component to render task cards

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,23 @@
+import { Task, TaskListProps } from "../types";
+import { TaskCard } from "./TaskCard";
+
+export const TaskList = ({
+  onTaskCheck,
+  onTaskDelete,
+  tasks,
+  title,
+}: TaskListProps) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      {tasks.map((task) => (
+        <TaskCard
+          key={task.id}
+          task={task}
+          onCheck={() => onTaskCheck(task.id)}
+          onDelete={() => onTaskDelete(task.id)}
+        />
+      ))}
+    </div>
+  );
+};
